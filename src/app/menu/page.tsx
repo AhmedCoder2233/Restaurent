d9a14ce92b7d35e5b2812d1580fa560cebd94a41
Ahmed Memon <ahmedpubgking3388@gmail.com>
2025-09-28 03:58:23 +0500

    Update page.tsx

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -54,7 +54,7 @@ export default function MenuPage() {
 
     const fetchMenu = async () => {
       try {
-        const res = await fetch("https://jessika-patrological-crankly.ngrok-free.dev/users/getmenu", {
+        const res = await fetch("https://myapiname.loca.lt/users/getmenu", {
           headers: { Authorization: `Bearer ${tokenData.token}` },
         });
         if (res.status === 403) {
@@ -341,6 +341,7 @@ export default function MenuPage() {
     </div>
   );
 }
+
 
 
 
